c9f700a777594044ad6f99944f0d57e13e71ecb0
Patrick Coxall <mr.coxall@mths.ca>
2020-10-04 13:14:44 -0700

    fixed calulation errors

--- main.ts
+++ main.ts
@@ -2,10 +2,11 @@
 // Created on: Oct 2020
 // 
 // This program calculated the area and perimeter of a rectangle.
+game.splash("Let's calculate the area and perimeter of a rectangle.")
 let length = game.askForNumber("Enter the length of rectangle (cm):")
 let width = game.askForNumber("Enter the width of rectangle (cm): ")
-let perimeter = length + width
+let perimeter = 2 * (length + width)
 game.splash("The perimeter of a rectangle dimensions: " + length + "cm by " + width + "cm is " + convertToText(perimeter) + " cm.")
-let area = 2 * (length * width)
-game.splash("The area of a rectangle dimensions: " + length + "cm by " + width + "cm is " + convertToText(area) + " cm.")
+let area = length * width
+game.splash("The area of a rectangle dimensions: " + length + "cm by " + width + "cm is " + convertToText(area) + " cm².")
 game.splash("Done")
