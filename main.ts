234a86146367918c9d5e36a6c83d61675bbbd264
Patrick Coxall <mr.coxall@mths.ca>
2020-10-04 13:08:33 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,11 @@
+// Created by: Mr. Coxall
+// Created on: Oct 2020
+// 
+// This program calculated the area and perimeter of a rectangle.
+let length = game.askForNumber("Enter the length of rectangle (cm):")
+let width = game.askForNumber("Enter the width of rectangle (cm): ")
+let perimeter = length + width
+game.splash("The perimeter of a rectangle dimensions: " + length + "cm by " + width + "cm is " + convertToText(perimeter) + " cm.")
+let area = 2 * (length * width)
+game.splash("The area of a rectangle dimensions: " + length + "cm by " + width + "cm is " + convertToText(area) + " cm.")
+game.splash("Done")
